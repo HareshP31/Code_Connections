2b720ab1d7d65a842117c5c21e5d7722bfd2c8de
Haresh Palli <realhareshpalli@gmail.com>
2025-04-06 19:04:31 -0400

    added links to about page

--- Code_Connections/src/pages/AboutPage.jsx
+++ Code_Connections/src/pages/AboutPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const AboutPage = () => {
     return (
         <div className="about-page">
             <h1>Welcome to Code Connections!</h1>
             <h2>This is a forum website made for hackathon team building (or CS projects in general).</h2>
-            <h2>Click "Home" to view posts or "Register" to get started!</h2>
+            <h2>Click <Link to="/home">here</Link> to view posts or <Link to="/register">here</Link> to get started!</h2>
         </div>
     );
 };
